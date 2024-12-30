09b68635a6224da6182937bca3338efd1cddad3b
haitan2408 <haitan28102408@gmail.com>
2024-12-30 11:40:25 +0700

    up demo ajax

--- src/main/java/com/codegym/c0824g1_spring_boot/controller/StudentRestController.java
+++ src/main/java/com/codegym/c0824g1_spring_boot/controller/StudentRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/students")
 public class StudentRestController {
 
